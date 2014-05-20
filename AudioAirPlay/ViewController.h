cdfc227d5d0dacec194969229a9e247396543426
13911256582@163.com <13911256582@163.com>
2014-05-21 07:58:42 +0800

    Initial Commit

---
+++ AudioAirPlay/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AudioAirPlay
+//
+//  Created by ShaoLing on 5/21/14.
+//  Copyright (c) 2014 dastone.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AudioAirPlayTests/AudioAirPlayTests.m
@@ -0,0 +1,34 @@
+//
+//  AudioAirPlayTests.m
+//  AudioAirPlayTests
+//
+//  Created by ShaoLing on 5/21/14.
+//  Copyright (c) 2014 dastone.cn. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AudioAirPlayTests : XCTestCase
+
+@end
+
+@implementation AudioAirPlayTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
